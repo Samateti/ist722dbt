83c41a4898dc4615fbde41f13d7ac91f52c3072c
Sathvik Mateti <samateti@g.syr.edu>
2024-03-08 04:12:37 +0000

    Finished Assignment-F

--- northwind/models/dim_product.sql
+++ northwind/models/dim_product.sql
@@ -1,20 +1,10 @@
 with stg_products as (
-    select
-        {{ dbt_utils.generate_surrogate_key(['productid']) }} as productkey,
-        productid,
-        {{ dbt_utils.generate_surrogate_key(['supplierid']) }} as supplierkey,
-        supplierid,
-        categoryid
-    from {{ source('northwind', 'Products') }}
+    select * from {{source('northwind','Products')}}
 ),
 stg_categories as (
-    select * from {{ source('northwind', 'Categories') }}
-)
-select 
-    stg.productkey,
-    stg.productid,
-    stg.supplierkey,
-    c.categoryname as categoryname,
-    c.description as categorydescription
-from stg_products stg
-left join stg_categories c on stg.categoryid = c.categoryid
+    select * from {{source('northwind','Categories')}}
+)    
+select  {{ dbt_utils.generate_surrogate_key(['p.productid']) }} as productkey, 
+    p.productid,p.productname,p.supplierid,c.categoryname,c.description as categorydescription
+    from stg_products p
+    join stg_categories c on p.categoryid = c.categoryid
--- northwind/models/obt_sales.sql
+++ northwind/models/obt_sales.sql
@@ -1,11 +1,11 @@
 with f_sales as (
     select * from {{ ref('fact_sales') }}
 ),
-d_customer as (
-    select * from {{ ref('dim_customer') }}
-),
 d_employee as (
     select * from {{ ref('dim_employee') }}
+),
+d_customer as (
+    select * from {{ ref('dim_customer') }}
 ),
 d_date as (
     select * from {{ ref('dim_date') }}
@@ -14,18 +14,13 @@ d_product as (
     select * from {{ ref('dim_product') }}
 )
 select 
-    d_customer.*, 
     d_employee.*, 
+    d_customer.*, 
     d_date.*,
     d_product.*,
-    f.orderid, f.orderdatekey, f.quantityonorder, f.extendedpriceamount, f.discountamount, f.soldamount
-from f_sales as f
-left join d_customer on f.customerkey = d_customer.customerkey
-left join d_employee on f.employeekey = d_employee.employeekey
-left join d_date on f.orderdatekey = d_date.datekey
-left join d_product on f.productkey = d_product.productkey
-
-
-
-
-
+    f.orderkey, f.orderdatekey, f.Quantity,f.extendedpriceamount,f.discountamount,f.soldamount
+    from f_sales as f
+    left join d_customer on f.customerkey = d_customer.customerkey
+    left join d_employee on f.employeekey = d_employee.employeekey
+    left join d_date on f.orderdatekey = d_date.datekey
+    left join d_product on f.productkey = d_product.productkey
